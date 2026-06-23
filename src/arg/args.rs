8896d00afae2b0e0d3da759d50703e6fda39a045
DouShaoxun <dousx_cn@163.com>
2026-06-23 09:46:27 +0800

    long_version

--- src/arg/args.rs
+++ src/arg/args.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
-
 #[derive(Parser, Debug)]
 #[command(
     name = "calculate sm3",
-    author = "DouShaoXun",
-    // 编译时注入版本号
+    author,                                   // 自动读取 Cargo.toml 的 authors
     version = env!("CARGO_PKG_VERSION"),
+    long_version = concat!(
+        "[",
+        env!("CARGO_PKG_VERSION"),
+        "] by ",
+        env!("CARGO_PKG_AUTHORS")
+    ),
 )]
 pub struct Args {
     /// file path
